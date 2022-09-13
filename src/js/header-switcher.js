894741b38a287ab575344ce0b071da845ed9341e
zaynlannister <zaynlannister@gmail.com>
2022-09-13 12:37:30 +0500

    rename variable

--- src/js/header-switcher.js
+++ src/js/header-switcher.js
@@ -1,13 +1,13 @@
-const items = document.querySelectorAll('.header-nav__item');
+const iconItems = document.querySelectorAll('.header-nav__item');
 
-items.forEach(item => {
+iconItems.forEach(item => {
     item.addEventListener('click', () => {
       switcher(item)
     })
 })
 
 function switcher(item) {
-    items.forEach(item => {
+    iconItems.forEach(item => {
         item.classList.remove('active')
     })
     item.classList.add('active')
