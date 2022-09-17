5502747b6c10ee80e1daa103f01e8ed56dc65335
zaynlannister <zaynlannister@gmail.com>
2022-09-17 23:32:06 +0500

    divide server response to another file

--- src/js/post-content.js
+++ src/js/post-content.js
@@ -1,28 +1,4 @@
-const responseFromServer = [
-    {
-        username: 'jonny123',
-        id: 1,
-        src: 'https://img2.akspic.ru/previews/4/8/5/9/6/169584/169584-malenkie_koshmary_2-malo_koshmary-dolgopyatnye_studii-mono_malenkie_koshmary-igra_uzhasov-x750.jpg',
-        likes: 124,
-        description: 'замечатльное фото...'
-    },
-
-    {
-        username: 'Lara_queen',
-        id: 2,
-        src: 'https://img2.akspic.ru/previews/4/8/5/9/6/169584/169584-malenkie_koshmary_2-malo_koshmary-dolgopyatnye_studii-mono_malenkie_koshmary-igra_uzhasov-x750.jpg',
-        likes: 12,
-        description: 'ууу'
-    },
-
-    {
-        username: 'oleg1mazelov',
-        id: 3,
-        src: 'https://img2.akspic.ru/previews/4/8/5/9/6/169584/169584-malenkie_koshmary_2-malo_koshmary-dolgopyatnye_studii-mono_malenkie_koshmary-igra_uzhasov-x750.jpg',
-        likes: 566,
-        description: 'spectacular view!'
-    }
-]
+import { responseFromServer } from "./server-response";
 
 class Post {
     constructor(props) {
@@ -77,3 +53,13 @@ class Post {
         `
     }
 }
+
+const data = [];
+
+function prepareData(responseFromServer) {
+    responseFromServer.forEach(item => {
+        data.push(new Post(item))
+    })
+}
+
+prepareData(responseFromServer);
