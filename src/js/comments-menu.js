2b9fa828e04dab5560966bcf7d01aa34c830becf
zaynlannister <zaynlannister@gmail.com>
2022-09-24 03:16:27 +0500

    add comments username rendering

--- src/js/comments-menu.js
+++ src/js/comments-menu.js
@@ -1,9 +1,11 @@
 import { renderComments } from "./post-comments";
 import { commentsButton } from "./post-comments";
+import {postMaterials} from "./post-content";
 
 const commentsMenu = document.querySelector('.comments');
 export const commentsContainer = document.querySelector('.comments__content');
 const postsContainer = document.querySelector('.presentation-section');
+const commentMenuUsername = document.querySelector('.user-post__username');
 
 postsContainer.addEventListener('click', (event) => {
     toggleCommentMenu(event);
@@ -16,6 +18,15 @@ function toggleCommentMenu(event) {
         commentsMenu.classList.toggle('active');
         renderComments(target.getAttribute('data-id'));
 
-        commentsButton.setAttribute('data-id', target.getAttribute('data-id'))
+        const id = parseInt(target.getAttribute('data-id'));
+
+        commentsButton.setAttribute('data-id', id);
+
+        const desiredPost = postMaterials.find(item => item.id === id);
+        renderCommentUsername(desiredPost);
     }
+}
+
+function renderCommentUsername(post) {
+    commentMenuUsername.innerHTML = post.username;
 }
